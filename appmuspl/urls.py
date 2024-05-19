4b1515b79034a9efca8b6f04445f2f509679a86e
aantenna <nitsuku@mail.ru>
2024-05-19 21:17:48 +0300

    Debug toolbar added

--- appmuspl/urls.py
+++ appmuspl/urls.py
@@ -17,9 +17,15 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import include, path
 
+from appmuspl.settings import DEBUG
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('main.urls', namespace='main')),
-    path('сategories/', include('tracks.urls', namespace='сategories'))
+    path('сategories/', include('tracks.urls', namespace='сategories')),
 ]
+
+if DEBUG:
+    urlpatterns += [
+        path("__debug__/", include("debug_toolbar.urls")),
+    ]
