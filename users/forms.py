ad6f46b0e997e9b1c6d7e5ecb6d5ad251badb229
aantenna <nitsuku@mail.ru>
2024-05-21 05:33:42 +0300

    Registration form and controller are ready

--- users/forms.py
+++ users/forms.py
@@ -31,24 +31,24 @@ class UserLoginForm(AuthenticationForm):
     # )
 
 
-# class UserRegistrationForm(UserCreationForm):
-#     class Meta:
-#         model = User
-#         fields = (
-#             "first_name",
-#             "last_name",
-#             "username",
-#             "email",
-#             "password1",
-#             "password2",
-#         )
-#
-#     first_name = forms.CharField()
-#     last_name = forms.CharField()
-#     username = forms.CharField()
-#     email = forms.CharField()
-#     password1 = forms.CharField()
-#     password2 = forms.CharField()
+class UserRegistrationForm(UserCreationForm):
+    class Meta:
+        model = User
+        fields = (
+            "first_name",
+            "last_name",
+            "username",
+            "email",
+            "password1",
+            "password2",
+        )
+
+    first_name = forms.CharField()
+    last_name = forms.CharField()
+    username = forms.CharField()
+    email = forms.CharField()
+    password1 = forms.CharField()
+    password2 = forms.CharField()
 
     # first_name = forms.CharField(
     #     widget=forms.TextInput(
--- users/views.py
+++ users/views.py
@@ -6,7 +6,7 @@ from django.shortcuts import redirect, render
 from django.urls import reverse
 
 
-from users.forms import UserLoginForm
+from users.forms import UserLoginForm, UserRegistrationForm
 
 
 def login(request):
@@ -32,9 +32,23 @@ def login(request):
     }
     return render(request, 'users/login.html', context)
 
+
 def registration(request):
+    if request.method == 'POST':
+        form = UserRegistrationForm(data=request.POST)
+        if form.is_valid():
+            form.save()
+
+            user = form.instance
+            auth.login(request, user)
+
+            return HttpResponseRedirect(reverse('main:index'))
+    else:
+        form = UserRegistrationForm()
+
     context = {
-        'title': 'Home - Регистрация'
+        'title': 'Home - Регистрация',
+        'form': form
     }
     return render(request, 'users/registration.html', context)
 
@@ -45,4 +59,5 @@ def profile(request):
     return render(request, 'users/profile.html', context)
 
 def logout(request):
-    ...
+    auth.logout(request)
+    return redirect(reverse('main:index'))
